75362f9d373017d431b511150d177e53ee61fdd5
Alpha <alpha.likembe@student.moringaschool.com>
2023-10-31 18:23:37 +0300

    Complete

--- lib/debug.py
+++ lib/debug.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-from config import CONN, CURSOR
-from song import Song
+# from config import CONN, CURSOR
+# from song import Song
+
+from lib import CONN, CURSOR
+from lib.song import Song
+
+import pytest; pytest.set_trace()
 
 
 if __name__ == '__main__':
